1f18ee84aa198628643b203a8512ed37ab5df232
Hugo <hugopc@dualboot.hp>
2022-12-12 23:05:04 +0100

    *

--- main.js
+++ main.js
@@ -4,20 +4,4 @@ const fs = require("fs");
 const config = require("./config.json");
 client.config = config;
 
-
-client.interactions = new Discord.Collection();
-client.register_arr = []
-fs.readdir("./slash/", (_err, files) => {
-    files.forEach(file => {
-        if (!file.endsWith(".js")) return;
-        let props = require(`./slash/${file}`);
-        let commandName = file.split(".")[0];
-        client.interactions.set(commandName, {
-            name: commandName,
-            ...props
-        });
-        client.register_arr.push(props)
-    });
-});
-
 client.login(config.token);
