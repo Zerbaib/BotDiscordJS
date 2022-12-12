132a260b9f4faeebfedaffcd3986860108115e38
Hugo <hugopc@dualboot.hp>
2022-12-12 23:13:25 +0100

    *

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
-const Discord = require("discord.js");
-const client = new Discord.Client({ intents: 7753 });
-const fs = require("fs");
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 const config = require("./config.json");
 client.config = config;
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
+});
 
 client.login(config.token);
